fc20640cf2d47818fd29fd097b361aea770ae282
Jared Backofen <backofen.jared@gmail.com>
2022-10-30 19:28:26 -0600

    added AWS config

--- modules/yfinance_example.py
+++ modules/yfinance_example.py
@@ -1,8 +1,18 @@
+import os
+import json
 import yfinance as yf
 import pandas as pd
 # import boto3
 import awswrangler as wr
 import datetime
+
+# Get AWS keys
+f = open('config.json')
+config = json.load(f)
+
+os.environ['AWS_ACCESS_KEY_ID'] = config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_DEFAULT_REGION'] = config['AWS_DEFAULT_REGION']
 
 def pullUploadFinanceData():
     tickers = yf.Tickers('MSFT META')
@@ -21,11 +31,11 @@ def pullUploadFinanceData():
     todays_date = datetime.datetime.today().strftime("%Y-%m-%d")
 
     # Storing data on Data Lake
-    # wr.s3.to_parquet(
-    #     df=data.copy(),
-    #     path="s3://yfinance-test/daily/",
-    #     dataset=True,
-    #     database="yfinance",
-    #     table="history_daily",
-    #     filename_prefix=todays_date
-    # )
+    wr.s3.to_parquet(
+        df=data.copy(),
+        path="s3://yfinance-test/daily/",
+        dataset=True,
+        database="yfinance",
+        table="history_daily",
+        filename_prefix=todays_date
+    )
